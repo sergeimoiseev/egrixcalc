67056e31be4df4f1fe04adb7f206422ed15e1385
sergeimoiseev <moiseev.sergei@gmail.com>
2016-01-31 11:20:39 +0300

    prev commit works - replacing filev step by step to get egrixcalc - replaced app.py, index.html, added empty egrix_calc.py

--- app.py
+++ app.py
@@ -1,75 +1,28 @@
-# all the imports
-import sqlite3
-from flask import Flask, request, session, g, redirect, url_for, \
-     abort, render_template, flash
+# -*- coding: utf-8 -*-
+import flask, flask.views
+import egrix_calc
 
-# configuration
-DATABASE = 'app.db'
-DEBUG = True
-SECRET_KEY = 'development key'
-USERNAME = 'admin'
-PASSWORD = 'default'
+app = flask.Flask(__name__)
+# Don't do this!
+app.secret_key = "bacon"
 
-# create our little application
-app = Flask(__name__)
-app.config.from_object(__name__)
+class View(flask.views.MethodView):
+    def get(self):
+        return flask.render_template('index.html')
+        
+    def post(self):
+        output_list = []
 
-def connect_db():
-    return sqlite3.connect(app.config['DATABASE'])
+        data = dict((key, flask.request.form.getlist(key)) for key in flask.request.form.keys())
+        data_keys = [key for key in flask.request.form.keys()]
+        data_vals = [flask.request.form.getlist(key) for key in  data_keys]
 
-from contextlib import closing
-def init_db():
-    with closing(connect_db()) as db:
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
+        flask.flash(data_keys)
+        flask.flash(data_vals)
 
-# sql3 requests decorators
-@app.before_request
-def before_request():
-    g.db = connect_db()
+        return self.get()
+    
+app.add_url_rule('/', view_func=View.as_view('main'), methods=['GET', 'POST'])
 
-@app.teardown_request
-def teardown_request(exception):
-    db = getattr(g, 'db', None)
-    if db is not None:
-        db.close()
-
-@app.route('/')
-def show_entries():
-    cur = g.db.execute('select title, text from entries order by id desc')
-    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
-    return render_template('show_entries.html', entries=entries)
-
-@app.route('/add', methods=['POST'])
-def add_entry():
-    if not session.get('logged_in'):
-        abort(401)
-    g.db.execute('insert into entries (title, text) values (?, ?)',
-                 [request.form['title'], request.form['text']])
-    g.db.commit()
-    flash('New entry was successfully posted')
-    return redirect(url_for('show_entries'))
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
-        else:
-            session['logged_in'] = True
-            flash('You were logged in')
-            return redirect(url_for('show_entries'))
-    return render_template('login.html', error=error)
-
-@app.route('/logout')
-def logout():
-    session.pop('logged_in', None)
-    flash('You were logged out')
-    return redirect(url_for('show_entries'))
-
-if __name__ == '__main__':
-    app.run()
+app.debug = True
+app.run()
